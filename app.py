ab1d33ca010f798502d45ca504e3c730f2207418
aaiit <abdellatif.aithammadi@gmail.com>
2021-08-25 10:05:56 +0200

    .

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
-from flask import Flask
+import io
+import json          
+import base64          
+import logging       
+import numpy as np
+from PIL import Image
+
+from flask import Flask, request, jsonify, abort
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return "Hello, World!"
+@app.route("/test", methods=['POST'])
+def hello_insta():
+   # print(request.json)     
+   if not request.json or 'image' not in request.json: 
+     abort(400)
+       
+   # get the base64 encoded string
+   im_b64 = request.json['image']
+
+   # convert it into bytes  
+   img_bytes = base64.b64decode(im_b64.encode('utf-8'))
+
+   # convert bytes data to PIL Image object
+   img = Image.open(io.BytesIO(img_bytes))
+
+   # PIL image object to numpy array
+   img_arr = np.asarray(img)   
+   print('img shape', img_arr.shape)
+
+   # process your img_arr here  
+   
+   # access other keys of json
+   print(request.json['token'])
+
+   result_dict = {'output': []}
+
+   for i in range(10):
+      result_dict["output"].append({"username":"coco","distance":33})
+
+
+   return result_dict
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=5000)
---
+++ test.py
@@ -0,0 +1,22 @@
+# https://stackoverflow.com/questions/29104107/upload-image-using-post-form-data-in-python-requests
+#  https://aws.amazon.com/fr/getting-started/hands-on/serve-a-flask-app/
+import base64
+import json
+import requests
+
+api = 'http://localhost:5000/test'
+image_file = 'sample_image.jpg'
+
+with open(image_file, "rb") as f:
+    im_bytes = f.read()        
+im_b64 = base64.b64encode(im_bytes).decode("utf8")
+
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+  
+payload = json.dumps({"image": im_b64, "token": "2001234444424"})
+response = requests.post(api, data=payload, headers=headers)
+try:
+    data = response.json()     
+    print(data)                
+except requests.exceptions.RequestException:
+    print(response.text)
